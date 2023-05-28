8b5cc8a2ea261dbc7e79d8ed5c915116bc8c174c
Cloud User <ec2-user@ip-172-31-93-48.ec2.internal>
2023-05-28 03:24:23 +0000

    my welcome file created

---
+++ welcome.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Good evening engineers"
+whoami
+echo "welcome to team13"
+echo "Thanks for joining us"
+echo "This day!"
+date
+echo " Is a remarkable day for us"
+echo "Enjoy your membership"
